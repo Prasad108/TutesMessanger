8907818a6ee0d197fdfc9eec52864a22f7160abf
A664162 <A664162@MC0XQ6KC.ww930.my-it-solutions.net>
2017-07-19 12:04:38 +0530

    added pojoes of institute, login, teacher ,role

--- src/main/java/com/app/controller/HelloController.java
+++ src/main/java/com/app/controller/HelloController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.app.DAO.iml.saveStudent;
-import com.app.pojo.Student;
+import com.app.pojo.Institute;
+import com.app.pojo.Login;
+
+
+import com.app.pojo.Teacher;
 
 
 
@@ -21,15 +25,23 @@ public class HelloController {
   
 		System.out.println("this is from hello controller");
 		
-		Student s= new Student("Prasad", "dukale");
-		int i= save.save(s);
+		
+		
+		
+		Teacher t= new Teacher("Prasad", "Dukale", "pdukale9@gmail.com", "9657939975");
+		save.saveTeacher(1, "Prasad", "Dukale", t);
+		
+		Login l= new Login("Prasad", "Dukale");
+		int i=save.findlogin(l);
+		
+		System.out.println("teacher saved");
 	    String message = "";  
 		if(i>0)
 		{
-			message+="Student saved";
+			message+="teacher saved";
 		}
 		else {
-			message+="error student not saved ";
+			message+="error teacher not saved ";
 		}
 		
     
