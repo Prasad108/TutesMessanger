1b4f87ff5ed397d99fcc8588d39d67bcd95a6ee4
prasad108 <prasad.dukale@atos.net>
2018-08-15 17:48:35 +0530

    Student SPA progress

---
+++ src/main/webapp/app/student/app.js
@@ -0,0 +1,16 @@
+(function(){
+	var app=angular.module('myApp',['ui.router']);
+		
+	app.config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider) {
+		  
+		$urlRouterProvider.otherwise('/Home');	
+		$stateProvider.state({name : 'Home', url : '/Home',	component : 'homeComponet'});	
+		$stateProvider.state({name : 'Schedule', url : '/Schedule', templateUrl: '../app/student/Schedule/Schedule.html', controller: 'ScheduleCtrl'});
+		
+	}]);
+
+
+
+
+}())
+
